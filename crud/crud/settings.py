95d6448ccab7be66be365caa70b1e69cc2217cc6
Sebastian Morales <51178433+SebaMorales74@users.noreply.github.com>
2025-09-27 21:14:33 -0300

    feat: vercel compatibility

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--l77@ykmfk902*6mi&e0t-^x7rsp!c#vh*^s@)^c9%ial)w-wp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
